df0275c0b6331b92c463f25df5a9d5f99bf7284e
edy alves <30.edyalves@gmail.com>
2024-11-24 16:33:04 -0300

    Initial Commit

---
+++ TaskManagerUI/TaskManagerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerUIApp.swift
+//  TaskManagerUI
+//
+//  Created by ednardo alves on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
