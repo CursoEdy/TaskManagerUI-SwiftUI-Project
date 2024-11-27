b45da32c83af6130e81b34458f118ac38b1fdb4d
edy alves <30.edyalves@gmail.com>
2024-11-26 23:10:48 -0300

    con centric Circles using foreach and scaleEffect

--- TaskManagerUI/MonthlyReviewView/TopAvatarView.swift
+++ TaskManagerUI/MonthlyReviewView/TopAvatarView.swift
@@ -8,13 +8,71 @@
 import SwiftUI
 
 struct TopAvatarView: View {
+    let circleSize: CGFloat = 55
+    
     var body: some View {
         HStack {
             ThreeLinesMenuBar(color: .white, lineWidth: 5, lineHeight: 30, stretch: 0.4, spacing: 6)
             Spacer()
-            CircleImageView(imageName: person1, color: .white, size: 55, lineWidth: 2)
+            ZStack {
+                
+                ForEach(0 ..< 20) { i in
+                    let factor = 2+1.5*CGFloat(i)
+                    CircleView(
+                        color: Purple2,
+                        lineWidth: 1 / factor,
+                        opacity: 2 / CGFloat(i),
+                        dim: circleSize,
+                        scaleEffect: factor
+                    )
+                }
+//                Circle()
+//                    .stroke(Purple2, lineWidth: 1)
+//                    .frame(width: circleSize*2, height: circleSize*2)
+//                Circle()
+//                    .stroke(Purple2, lineWidth: 1)
+//                    .frame(width: circleSize*2, height: circleSize*2)
+//                    .scaleEffect(1.5)
+//                Circle()
+//                    .stroke(Purple2, lineWidth: 1)
+//                    .frame(width: circleSize*2, height: circleSize*2)
+//                    .scaleEffect(2)
+//                Circle()
+//                    .stroke(Purple2, lineWidth: 1)
+//                    .frame(width: circleSize*2, height: circleSize*2)
+//                    .scaleEffect(2.5)
+//                Circle()
+//                    .stroke(Purple2, lineWidth: 1)
+//                    .frame(width: circleSize*2, height: circleSize*2)
+//                    .scaleEffect(3)
+//                Circle()
+//                    .stroke(Purple2, lineWidth: 1)
+//                    .frame(width: circleSize*2, height: circleSize*2)
+//                    .scaleEffect(3.5)
+//                Circle()
+//                    .stroke(Purple2, lineWidth: 1)
+//                    .frame(width: circleSize*2, height: circleSize*2)
+//                    .scaleEffect(4)
+                
+                CircleImageView(imageName: person1, color: .white, size: circleSize, lineWidth: 2)
+            }
         }
         .padding()
+    }
+}
+
+struct CircleView: View {
+    let color: Color
+    let lineWidth: CGFloat
+    let opacity: CGFloat
+    let dim: CGFloat
+    let scaleEffect: CGFloat
+    
+    var body: some View {
+        Circle()
+            .stroke(color.opacity(opacity), lineWidth: lineWidth)
+            .frame(width: dim, height: dim)
+            .scaleEffect(scaleEffect)
     }
 }
 
